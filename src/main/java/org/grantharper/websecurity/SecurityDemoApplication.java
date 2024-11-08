92eaaad5c44273ca37843c738a61fd96e3c05904
Duncan McIntyre <duncan.mcintyre@snyk.io>
2024-11-08 15:18:32 +0000

    Removed pointless println

--- src/main/java/org/grantharper/websecurity/SecurityDemoApplication.java
+++ src/main/java/org/grantharper/websecurity/SecurityDemoApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecurityDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("");
 		SpringApplication.run(SecurityDemoApplication.class, args);
 	}
 }
